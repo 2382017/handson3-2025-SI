8ec13d9dae1471b85c85218c0a91ee3c1931cd38
Rawely Naibaho <2382017@unai.edu>
2025-02-05 12:55:32 +0700

    handson3

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <table>
+        <tr>
+          <th>NIM</th>
+          <th>Nama</th>
+          <th>Jurusan</th>
+          <th>Email</th>
+          <th>Hobby</th>
+        </tr>
+
+        <tr>
+          <td>2382003</td>
+          <td>Raweli Naibaho</td>
+          <td>Sistem Informasi</td>
+          <td>2382017@unai.edu</td>
+          <td>Volly</td>
+        </tr>
+      </table>
+    </>
+  )
+}
+
+export default App
